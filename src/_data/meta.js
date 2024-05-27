aa1c16776b0529ea06eabab91b3bacee069ccd75
Atomic Junky <67459553+atomic-junky@users.noreply.github.com>
2024-05-27 15:51:38 +0200

    favicons and webmanifest

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,10 +1,10 @@
 module.exports = {
 	title: "La Confrérie D6 Maîtres",
-	description: "I am writing about my experiences as a naval navel-gazer.",
-	url: process.env.URL || "http://localhost:8080/",
+	description: "La Confrérie D6 Maîtres : l'association Jeu de Rôle de Fontenay-sous-Bois",
+	url: process.env.CONFIG_BASE_URL || "http://localhost:8080/",
 	language: "fr",
 	locale: "Fr-fr",
 	author: "AtomicJunky",
-	image: '/assets/images/site.og-image.png',
+	image: './assets/images/site.og-image.png',
 	themeColor: '#FFFFFF'
 }
