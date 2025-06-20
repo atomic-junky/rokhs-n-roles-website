81c8a19f574910d826116bdab1a43bca090f3d53
Atomic Junky <67459553+atomic-junky@users.noreply.github.com>
2025-06-20 04:39:48 +0200

    netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import react from '@astrojs/react';
 import { loadEnv } from "vite";
 
 
+import netlify from '@astrojs/netlify';
+
+
 const { SANITY_PROJECT_ID } = loadEnv(process.env.SANITY_PROJECT_ID, process.cwd(), "");
 const { SANITY_DATASET } = loadEnv(process.env.SANITY_DATASET, process.cwd(), "");
 const { SANITY_API_VERSION } = loadEnv(process.env.SANITY_API_VERSION, process.cwd(), "");
@@ -24,13 +27,15 @@ const { SANITY_USE_CDN } = loadEnv(process.env.SANITY_USE_CDN, process.cwd(), ""
 // });
 
 export default defineConfig({
-    integrations: [sanity({
-        projectId: 'iqveg3px',
-        dataset: 'production',
+  integrations: [sanity({
+      projectId: 'iqveg3px',
+      dataset: 'production',
 
-        useCdn: false,
-        studioBasePath: '/admin',   
-		}),
-        react()
-    ],
+      useCdn: false,
+      studioBasePath: '/admin',   
+      }),
+      react()
+  ],
+
+  adapter: netlify(),
 });
