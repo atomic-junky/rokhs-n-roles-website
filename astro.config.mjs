cdb0cf1fc4a6bc3011873d02e179b2b8aba5b856
Atomic Junky <67459553+atomic-junky@users.noreply.github.com>
2025-06-20 04:33:51 +0200

    Some new things idk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-// @ts-nocheck
 import { defineConfig } from 'astro/config';
-import sanity from '@sanity/astro'
-import vue  from '@astrojs/vue'
-import vercel from '@astrojs/vercel'
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
 
 import { loadEnv } from "vite";
+
 
 const { SANITY_PROJECT_ID } = loadEnv(process.env.SANITY_PROJECT_ID, process.cwd(), "");
 const { SANITY_DATASET } = loadEnv(process.env.SANITY_DATASET, process.cwd(), "");
@@ -12,17 +11,26 @@ const { SANITY_API_VERSION } = loadEnv(process.env.SANITY_API_VERSION, process.c
 const { SANITY_USE_CDN } = loadEnv(process.env.SANITY_USE_CDN, process.cwd(), "");
 
 
+// export default defineConfig({
+//     integrations: [sanity({
+//         projectId: SANITY_PROJECT_ID,
+//         dataset: SANITY_DATASET || 'production',
+
+//         useCdn: SANITY_USE_CDN || false,
+//         studioBasePath: '/admin',   
+// 		}),
+//         react()
+//     ],
+// });
+
 export default defineConfig({
-	integrations: [
-		sanity({
-			projectId: SANITY_PROJECT_ID,
-			dataset: SANITY_DATASET || 'production',
-			apiVersion: SANITY_API_VERSION || '2022-07-27',
-			useCdn: SANITY_USE_CDN || false,
-      		studioBasePath: '/admin',
+    integrations: [sanity({
+        projectId: 'iqveg3px',
+        dataset: 'production',
+
+        useCdn: false,
+        studioBasePath: '/admin',   
 		}),
-    	vue(),
-	],
-	output: 'server',
-	adapter: vercel(),
+        react()
+    ],
 });
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,23 +1,17 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import { presentationTool } from 'sanity/presentation'
-import {schemaTypes} from './schemas'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
   title: 'Rokhs & Rôles',
+
   projectId: import.meta.env.SANITY_PROJECT_ID,
   dataset: import.meta.env.SANITY_DATASET || 'production',
-  plugins: [structureTool(), presentationTool({
-      previewUrl: {
-        origin: import.meta.env.BASE_URL,
-        previewMode: {
-          enable: '/api/draft-mode/enable',
-          disable: '/api/draft-mode/disable',
-        },
-      },
-    }),
-],
+
+  plugins: [structureTool(), visionTool()],
+
   schema: {
     types: schemaTypes,
   },
