cec71d5c140c6d904db18d8580bb7d3941c0ca6d
Atomic Junky <67459553+atomic-junky@users.noreply.github.com>
2025-09-10 22:29:54 -0400

    Use Astro Image component to improve image performance

--- src/lib/sanityPortableText.jsx
+++ src/lib/sanityPortableText.jsx
@@ -1,9 +1,10 @@
+import { Image } from 'astro:assets';
 import { getSanityImageURL } from '../utils/helpers.js';
 
 export const imageType = ({ value }) => {
     return (
         <div class="content-image-container">
-            <img src={getSanityImageURL(value.asset).url()} />
+            <Image src={getSanityImageURL(value.asset).url()} />
             <br/>
         </div>
     )
