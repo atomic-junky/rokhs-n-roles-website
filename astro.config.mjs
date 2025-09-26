5ab62adc4db985c1dd9199fa734647e77319bd85
Atomic Junky <67459553+atomic-junky@users.noreply.github.com>
2025-09-26 14:00:17 -0400

    Remove astro-purgecss and update blog post styles
    astro-purgecss has been removed from dependencies and configuration to simplify the build process. Additionally, global styles for the blog content section have been added to improve layout and control spacing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import purgecss from 'astro-purgecss';
 import icon from "astro-icon";
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
@@ -11,7 +10,6 @@ import { loadEnv } from "vite";
 const { CONFIG_BASE_URL } = loadEnv(process.env.CONFIG_BASE_URL, process.cwd(), "")
 const { SANITY_PROJECT_ID } = loadEnv(process.env.SANITY_PROJECT_ID ?? "", process.cwd(), "");
 const { SANITY_DATASET } = loadEnv(process.env.SANITY_DATASET ?? "", process.cwd(), "");
-// const { SANITY_API_VERSION } = loadEnv(process.env.SANITY_API_VERSION ?? "", process.cwd(), "");
 const { SANITY_USE_CDN } = loadEnv(process.env.SANITY_USE_CDN ?? "", process.cwd(), "");
 
 
@@ -22,13 +20,6 @@ export default defineConfig({
 
       useCdn: SANITY_USE_CDN || false,
       studioBasePath: '/admin',   
-      }),
-      purgecss({
-        fontFace: true,
-        keyframes: true,
-        content: [
-          process.cwd() + '/src/**/*.{astro}'
-        ],
       }),
       icon(),
       react(),
